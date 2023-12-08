9822742df6a5867b41b6af41aa2f98c2f2e4ecc9
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2023-12-08 19:00:46 +0100

    adds funcitonality to search for CLI in 3 places, changed node's names

---
+++ addons/rivet_api/rivet_cli.gd
@@ -0,0 +1,53 @@
+@tool
+## A brief description of the class's role and functionality.
+##
+## The description of the script, what it can do,
+## and any further detail.
+##
+## @tutorial:            https://the/tutorial1/url.com
+## @tutorial(Tutorial2): https://the/tutorial2/url.com
+## @experimental
+
+const RIVET_CLI_PATH_SETTING = "rivet/CLI_executable_path"
+
+#region Utilities
+static func find_executable(program: String) -> String:
+	var os := OS.get_name()
+	var output = []
+	var code: int = -1
+	if OS.get_name() == "Windows":
+		code = OS.execute("where", [program], output, true)
+	else:
+		code = OS.execute("where", [program], output, true)
+	
+	if code == 1 or output.size() < 1:
+		return ""
+	return output[0].strip_escapes()
+
+static func find_rivet():
+	var editor_rivet_path = EditorInterface.get_editor_settings().get_setting(RIVET_CLI_PATH_SETTING)
+	if not editor_rivet_path.is_empty():
+		return editor_rivet_path
+	printerr("Can't find path to Rivet CLI (in editor settings)")
+	
+	var rivet_path = find_executable("rivet")
+	if not rivet_path.is_empty():
+		return rivet_path
+	printerr("Can't find path to Rivet CLI (rivet exec)")
+	
+	var rivet_cli_path = find_executable("rivet-cli")
+	if not rivet_cli_path.is_empty():
+		return rivet_cli_path
+	print(rivet_path, rivet_cli_path)
+	printerr("Can't find path to Rivet CLI (rivet-cli exec)")
+	
+
+static func execute(args: PackedStringArray):
+	var output = []
+	var code = OS.execute(find_rivet(), args, output, true)
+	return [code, output]
+#endregion
+	
+static func link():
+	# TODO(forest): please edit this part with appropiate command
+	var result = execute(["login"])
