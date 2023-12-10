3d992e5003f246d3f9bfb14b23ace1be6e45a74a
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2023-12-11 00:25:55 +0100

    fix invalid alias

--- addons/rivet/rivet_global.gd
+++ addons/rivet/rivet_global.gd
@@ -4,9 +4,9 @@ extends Node
 const _api = preload("api/rivet_api.gd")
 
 ## @experimental
-func POST(owner: Node, path: String, body: Dictionary) -> _api.RivetRequest:
+func POST(path: String, body: Dictionary) -> _api.RivetRequest:
 	return _api.POST(self, path, body)
 
 ## @experimental
-func GET(owner: Node, path: String, body: Dictionary) -> _api.RivetRequest:
+func GET(path: String, body: Dictionary) -> _api.RivetRequest:
 	return _api.GET(self, path, body)
