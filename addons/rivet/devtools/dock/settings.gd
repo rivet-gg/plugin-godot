c4c018b152cea419b88a7cb454cc3fde68408489
Forest Anderson <forestkzanderson@gmail.com>
2023-12-15 12:03:07 -0500

    Manage version functionality

--- addons/rivet/devtools/dock/settings.gd
+++ addons/rivet/devtools/dock/settings.gd
@@ -3,6 +3,9 @@
 
 # TODO
 const GAME_ID = "YOUR GAME ID HERE"
+
+func _ready():
+	%ManageVersions.pressed.connect(_on_manage_version_button_pressed)
 	
 func prepare():
 	var request := RivetDevtools.get_plugin().GET("/cloud/games/%s" % GAME_ID).request()
@@ -30,3 +33,22 @@ func _populate_namespace_data(data: Object) -> void:
 	
 	%AuthNamespaceSelector.namespaces = namespaces
 	%DeployNamespaceSelector.namespaces = namespaces
+
+func _on_manage_version_button_pressed() -> void:
+		%ManageVersions.disabled = true
+		var result := await RivetDevtools.get_plugin().cli.run_command([
+			"--api-endpoint",
+			"https://api.staging2.gameinc.io",
+			"sidekick",
+			"get-version",
+			"--namespace",
+			# TODO: This needs to change to the selected namespace
+			"1b06ae8b-c799-4836-a27e-86cdb29223f9",
+		])
+
+		if result.exit_code == result.ExitCode.SUCCESS and result.output.has("Ok"):
+			var data: Dictionary = result.output["Ok"]
+
+			OS.shell_open(data["output"])
+
+		%ManageVersions.disabled = false
