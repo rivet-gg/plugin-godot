d3a2a78a3235198b5af1b55205f30ce8da689607
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2023-12-19 03:19:09 +0100

    Redesign of the last screen

--- addons/rivet/devtools/dock/settings.gd
+++ addons/rivet/devtools/dock/settings.gd
@@ -1,81 +1,10 @@
 @tool extends Control
 ## Settings screens allow you to configure and deploy your game.
 
-@onready var AuthNamespaceSelector = %AuthNamespaceSelector
-@onready var ConnectionMenuButton: MenuButton = %ConnectionMenuButton
-@onready var DeployNamespaceSelector = %DeployNamespaceSelector
-@onready var BuildDeployButton: Button = %BuildDeployButton
-@onready var ManageVersionButton: Button = %ManageVersionButton
 @onready var ErrorDialog: AcceptDialog = %ErrorDialog
-
-func _ready():
-	ManageVersionButton.pressed.connect(_on_manage_version_button_pressed)
-	DeployNamespaceSelector.selected.connect(_on_deploy_namespace_selector_selected)
 	
 func prepare():
-	disable_all_actions(true)
-	
-	var error = await RivetDevtools.bootstrap()
+	var error = await RivetPluginBridge.instance.bootstrap()
 	if error:
 		ErrorDialog.popup_centered()
 		return
-
-	_fetch_data()
-
-	disable_all_actions(false)
-
-func _fetch_data():
-	var request := RivetDevtools.get_plugin().GET("/cloud/games/%s" % RivetDevtools.get_plugin().game_id).request()
-	var response = await request.wait_completed()
-	# response.body:
-	#	game.namespaces = {namespace_id, version_id, display_name}[]
-	#	game.versions = {version_id, display_name}[]
-	if response.response_code != HTTPClient.ResponseCode.RESPONSE_OK:
-		push_error("Something is not right")
-		return
-	_populate_namespace_data(response)
-
-func _populate_namespace_data(data: Object) -> void:
-	var namespaces = data.body.game.namespaces
-	
-	for space in namespaces:
-		var versions: Array = data.body.game.versions.filter(
-			func (version): return version.version_id == space.version_id
-		)
-		
-		if versions.is_empty():
-			space["version"] = null
-		else:
-			space["version"] = versions[0]
-	
-	AuthNamespaceSelector.namespaces = namespaces
-	DeployNamespaceSelector.namespaces = namespaces
-
-func _on_manage_version_button_pressed() -> void:
-		ManageVersionButton.disabled = true
-		var result := await RivetDevtools.get_plugin().cli.run_command([
-			"--api-endpoint",
-			RivetDevtools.get_plugin().api_endpoint,
-			"sidekick",
-			"get-version",
-			"--namespace",
-			DeployNamespaceSelector.current_value.version_id,
-		])
-
-		if result.exit_code == result.ExitCode.SUCCESS and result.output.has("Ok"):
-			var data: Dictionary = result.output["Ok"]
-
-			OS.shell_open(data["output"])
-
-		ManageVersionButton.disabled = false
-
-func _on_deploy_namespace_selector_selected():
-	ManageVersionButton.disabled = false
-	BuildDeployButton.disabled = false
-
-func disable_all_actions(disabled: bool) -> void:
-	AuthNamespaceSelector.disabled = disabled
-	ConnectionMenuButton.disabled = disabled
-	DeployNamespaceSelector.disabled = disabled
-	BuildDeployButton.disabled = disabled
-	ManageVersionButton.disabled = disabled
---
+++ addons/rivet/devtools/rivet_plugin_bridge.gd
@@ -0,0 +1,72 @@
+@tool class_name RivetPluginBridge
+## Scaffolding for the plugin to be used in the editor, this is not meant to be
+## used in the game. It's a way to get the plugin instance from the engine's
+## perspective.
+##
+## @experimental
+
+signal bootstrapped
+
+const _global := preload("../rivet_global.gd")
+
+static var instance = RivetPluginBridge.new()
+
+static func _find_plugin():
+	var tree: SceneTree = Engine.get_main_loop()
+	return tree.get_root().get_child(0).get_node_or_null("RivetPlugin")
+
+## Autoload is not available for editor interfaces, we add a scoffolding to get
+## the instance of the plugin from the engine's perspective
+## @experimental
+static func get_plugin() -> _global:
+	var plugin = _find_plugin()
+	if plugin:
+		return plugin.global
+	push_error("Can't find Rivet Plugin")
+	return null
+
+static var game_namespaces: Array
+
+func bootstrap() -> Error:
+	var plugin = get_plugin()
+	if not plugin:
+		return FAILED
+
+	var result = await get_plugin().cli.run_command([
+		"sidekick",
+		"get-bootstrap-data",
+	])
+
+	if result.exit_code != 0 or !("Ok" in result.output):
+		return FAILED
+	
+	get_plugin().api_endpoint = result.output["Ok"].api_endpoint
+	get_plugin().cloud_token = result.output["Ok"].token
+	get_plugin().game_id = result.output["Ok"].game_id
+
+	var fetch_result = await _fetch_plugin_data()
+	if fetch_result == OK:
+		emit_signal("bootstrapped")
+	return fetch_result
+
+func _fetch_plugin_data() -> Error:
+	var request = get_plugin().GET("/cloud/games/%s" % get_plugin().game_id).request()
+	var response = await request.wait_completed()
+	# response.body:
+	#	game.namespaces = {namespace_id, version_id, display_name}[]
+	#	game.versions = {version_id, display_name}[]
+	if response.response_code != HTTPClient.ResponseCode.RESPONSE_OK:
+		return FAILED
+	
+	var namespaces = response.body.game.namespaces
+	for space in namespaces:
+		var versions: Array = response.body.game.versions.filter(
+			func (version): return version.version_id == space.version_id
+		)
+		if versions.is_empty():
+			space["version"] = null
+		else:
+			space["version"] = versions[0]
+
+	game_namespaces = namespaces
+	return OK
