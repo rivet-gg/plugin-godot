beb761302c1eb741f540f91ad1a174e4cbd70cd1
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2023-12-24 00:08:28 +0100

    Adds unlink functionality

---
+++ addons/rivet/devtools/dock/settings_tab.gd
@@ -0,0 +1,23 @@
+@tool extends Control
+
+@onready var unlink_game_button: Button = %UnlinkGameButton
+
+
+func _ready() -> void:
+	unlink_game_button.pressed.connect(_on_unlink_game_button_pressed)
+
+
+func _on_unlink_game_button_pressed() -> void:
+	unlink_game_button.disabled = true
+
+	var result = await RivetPluginBridge.get_plugin().cli.run_command([
+		"unlink"
+	])
+
+	if result.exit_code != result.ExitCode.SUCCESS:
+		RivetPluginBridge.display_cli_error(self, result)
+		unlink_game_button.disabled = false
+		return
+
+	unlink_game_button.disabled = false
+	owner.owner.change_current_screen(owner.owner.Screen.Login)
