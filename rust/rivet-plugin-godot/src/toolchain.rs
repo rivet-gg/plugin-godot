3f8c45f42eb0ed848990fcc99fe2dbce6bca4d8b
Nathan Flurry <developer@nathanflurry.com>
2024-08-28 01:55:15 -0700

    chore: migrate output to read from output file

--- rust/rivet-plugin-godot/src/toolchain.rs
+++ rust/rivet-plugin-godot/src/toolchain.rs
@@ -19,14 +19,28 @@ impl IRefCounted for RivetToolchain {
 #[godot_api]
 impl RivetToolchain {
     #[func]
-    fn run_task(&mut self, run_config: String, name: String, input_json: String) -> String {
+    fn run_task(
+        &mut self,
+        run_config: String,
+        name: String,
+        input_json: String,
+        on_output_event: Callable,
+    ) -> String {
         let run_config = serde_json::from_str::<RunConfig>(&run_config).unwrap();
 
         let task_config = toolchain::tasks::get_task_config(&name);
 
         let name_inner = name.clone();
         let output_json = block_on(
-            async move { toolchain::tasks::run_task_json(run_config, &name_inner, &input_json).await },
+            async move {
+                toolchain::tasks::run_task_json(
+                    run_config,
+                    &name_inner,
+                    &input_json,
+                    move |event_json| output.callv(event_json),
+                )
+                .await
+            },
             BlockOnOpts {
                 multithreaded: task_config.prefer_multithreaded,
             },
