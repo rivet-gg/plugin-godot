67b6da2d5f580cf6f94a805c7339c918a6f2b1eb
NathanFlurry <git@nathanflurry.com>
2024-09-10 12:34:47 +0000

    chore: add shared tokio runtime (#205)

--- rust/rivet-plugin-godot/src/toolchain.rs
+++ rust/rivet-plugin-godot/src/toolchain.rs
@@ -2,7 +2,7 @@ use godot::prelude::*;
 use tokio::task::block_in_place;
 use toolchain::util::task;
 
-use crate::util::runtime::{block_on, BlockOnOpts};
+use crate::util::runtime::block_on;
 
 #[derive(GodotClass)]
 #[class(base=RefCounted)]
@@ -22,37 +22,34 @@ impl RivetToolchain {
     #[func]
     fn run_task(&mut self, name: String, input_json: String, on_output_event: Callable) {
         let name_inner = name.clone();
-        block_on(
-            async move {
-                let (run_config, mut handles) = task::RunConfig::build();
+        block_on(async move {
+            let (run_config, mut handles) = task::RunConfig::build();
 
-                // TODO: Add aborter
+            // TODO: Add aborter
 
-                // Spawn task
-                tokio::task::spawn(async move {
-                    toolchain::tasks::run_task_json(run_config, &name_inner, &input_json).await
+            // Spawn task
+            tokio::task::spawn(async move {
+                toolchain::tasks::run_task_json(run_config, &name_inner, &input_json).await
+            });
+
+            // Pass events to Godot
+            //
+            // Do this in the current thread since `Callable` is not `Send`
+            while let Some(event) = handles.event_rx.recv().await {
+                block_in_place(|| {
+                    // Serialize event
+                    let event_json = match serde_json::to_string(&event) {
+                        Ok(x) => x,
+                        Err(err) => {
+                            eprintln!("error with event: {err:?}");
+                            return;
+                        }
+                    };
+
+                    // Call Godot
+                    on_output_event.callv(godot::builtin::array![Variant::from(event_json)]);
                 });
-
-                // Pass events to Godot
-                //
-                // Do this in the current thread since `Callable` is not `Send`
-                while let Some(event) = handles.event_rx.recv().await {
-                    block_in_place(|| {
-                        // Serialize event
-                        let event_json = match serde_json::to_string(&event) {
-                            Ok(x) => x,
-                            Err(err) => {
-                                eprintln!("error with event: {err:?}");
-                                return;
-                            }
-                        };
-
-                        // Call Godot
-                        on_output_event.callv(godot::builtin::array![Variant::from(event_json)]);
-                    });
-                }
-            },
-            BlockOnOpts {},
-        );
+            }
+        });
     }
 }
--- rust/rivet-plugin-godot/src/util/runtime.rs
+++ rust/rivet-plugin-godot/src/util/runtime.rs
@@ -1,28 +1,22 @@
+use lazy_static::lazy_static;
 use std::future::Future;
-use tokio::time::Duration;
 
-pub struct BlockOnOpts {}
-
-/**
-* Create a temporary Tokio runtime to run the given future.
-*/
-pub fn block_on<Output>(fut: impl Future<Output = Output>, _opts: BlockOnOpts) -> Output {
-    // Build temporary runtime
-    //
-    // Multithreaded is required in order to be able to use blocking threads. We reduce the worker
-    // thread count to reduce footprint.
-    let rt = tokio::runtime::Builder::new_multi_thread()
+lazy_static! {
+    /// Shared runtime for executing tasks.
+    ///
+    /// Multithreaded is required in order to be able to use blocking threads. We reduce the worker
+    /// thread count to reduce footprint.
+    ///
+    /// Sharing the runtime instead of creating an independent runtiem for each task reduces memory
+    /// footprint.
+    static ref RUNTIME: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(2)
         .enable_all()
         .build()
         .unwrap();
+}
 
-    // Run future
-    let output = rt.block_on(fut);
-
-    // Give tasks time to shut down
-    // rt.shutdown_background();
-    rt.shutdown_timeout(Duration::from_secs(1));
-
-    output
+/// Runs a future on the shared runtime.
+pub fn block_on<Output>(fut: impl Future<Output = Output>) -> Output {
+    RUNTIME.block_on(fut)
 }
