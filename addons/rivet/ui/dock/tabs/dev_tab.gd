f935e5a80f2448efe88c892c040ffc84e3b551d9
NathanFlurry <git@nathanflurry.com>
2024-09-17 04:27:06 +0000

    chore: update client count to open native customize window (#231)

--- addons/rivet/ui/dock/tabs/dev_tab.gd
+++ addons/rivet/ui/dock/tabs/dev_tab.gd
@@ -8,11 +8,21 @@ const _RivetGlobal = preload("../../../rivet_global.gd")
 
 # Play
 @onready var _play_type_option: OptionButton = %PlayTypeOption
-@onready var _client_count_slider: Slider = %ClientCountSlider
-@onready var _client_count_label: Label = %ClientCountLabel
+# @onready var _client_count_slider: Slider = %ClientCountSlider
+# @onready var _client_count_label: Label = %ClientCountLabel
+
 @onready var _gs_start: Button = %GSStart
 @onready var _gs_stop: Button = %GSStop
 @onready var _gs_restart: Button = %GSRestart
+
+# Will be null on Godot < 4.3
+#
+# We can't manually update these settings since the rest of the engine reads
+# the instance count config directly from this UI
+#
+# See internals:
+# https://github.com/godotengine/godot/blob/48403b5358c11ffff702da82c48464db8c536ee3/editor/run_instances_dialog.h
+var _instances_dialog: AcceptDialog = null
 
 func _ready() -> void:
 	if get_tree().edited_scene_root == self:
@@ -41,20 +51,25 @@ func _ready() -> void:
 	# Play
 	RivetPluginBridge.get_plugin().game_server_state_change.connect(_on_gs_state_change)
 
-	# TODO: Play type
-
 	_play_type_option.item_selected.connect(_update_play_type)
-	_client_count_slider.value_changed.connect(_update_client_count)
+	# _client_count_slider.value_changed.connect(_update_client_count)
 
 	_gs_start.pressed.connect(_on_gs_start_pressed)
 	_gs_stop.pressed.connect(_on_gs_stop_pressed)
 	_gs_restart.pressed.connect(_on_gs_start_pressed)
 
+	%CustomizeInstances.pressed.connect(_on_customize_instances_pressed)
 	%ServerLogs.pressed.connect(_on_gs_logs_pressed)
 
-	_on_client_count_change.call_deferred()
+	# _on_client_count_change.call_deferred()
 	_on_gs_state_change.call_deferred(false)
 	_update_play_type.call_deferred(0)
+	
+	# Find instances dailog
+	for child in EditorInterface.get_base_control().get_children():
+		if child.is_class("RunInstancesDialog"):
+			_instances_dialog = child
+			break
 
 func _on_bootstrapped() -> void:
 	_update_selected_env()
@@ -102,46 +117,48 @@ var _enable_play_server: bool:
 		return _play_type_option.selected == 0 || _play_type_option.selected == 2
 	
 func _update_play_type(_id: int):
-	%ClientConfigContainer.visible = _enable_play_client
+	%CustomizeInstances.visible = _enable_play_client && _instances_dialog != null
+	%ServerLogs.visible = _enable_play_server
+	# %ClientConfigContainer.visible = _enable_play_client
 
-func _update_client_count(count: float):
-	# Convert to int
-	count = round(count)
-
-	var editor_interface = EditorInterface.get_editor_settings()
-	if count == 1:
-		editor_interface.set_project_metadata("debug_options", "multiple_instances_enabled", false)
-	else:
-		editor_interface.set_project_metadata("debug_options", "multiple_instances_enabled", true)
-		var run_config: Array = editor_interface.get_project_metadata("debug_options", "run_instances_config")
-		var default_entry = { "arguments": "", "features": "", "override_args": false, "override_features": false }
-		
-		# Add entries if needed
-		while run_config.size() < count:
-			run_config.append(default_entry.duplicate())
-		
-		# Remove entries if needed
-		while run_config.size() > count:
-			run_config.pop_back()
-		
-		editor_interface.set_project_metadata("debug_options", "run_instances_config", run_config)
-	
-	_on_client_count_change()
-
-func _on_client_count_change():
-	# Read config
-	var editor_interface = EditorInterface.get_editor_settings()
-	var instances_enabled: bool = editor_interface.get_project_metadata("debug_options", "multiple_instances_enabled")
-	var run_config: Array = editor_interface.get_project_metadata("debug_options", "run_instances_config")
-
-	var count = 0
-	if instances_enabled:
-		count = run_config.size()
-	else:
-		count = 1
-
-	_client_count_slider.value = count
-	_client_count_label.text = "%s" % count
+# func _update_client_count(count: float):
+# 	# Convert to int
+# 	count = round(count)
+# 	
+# 	var editor_interface = EditorInterface.get_editor_settings()
+# 	if count == 1:
+# 		editor_interface.set_project_metadata("debug_options", "multiple_instances_enabled", false)
+# 	else:
+# 		editor_interface.set_project_metadata("debug_options", "multiple_instances_enabled", true)
+# 		var run_config: Array = editor_interface.get_project_metadata("debug_options", "run_instances_config")
+# 		var default_entry = { "arguments": "", "features": "", "override_args": false, "override_features": false }
+# 		
+# 		# Add entries if needed
+# 		while run_config.size() < count:
+# 			run_config.append(default_entry.duplicate())
+# 		
+# 		# Remove entries if needed
+# 		while run_config.size() > count:
+# 			run_config.pop_back()
+# 		
+# 		editor_interface.set_project_metadata("debug_options", "run_instances_config", run_config)
+# 	
+# 	_on_client_count_change()
+#
+# func _on_client_count_change():
+# 	# Read config
+# 	var editor_interface = EditorInterface.get_editor_settings()
+# 	var instances_enabled: bool = editor_interface.get_project_metadata("debug_options", "multiple_instances_enabled")
+# 	var run_config: Array = editor_interface.get_project_metadata("debug_options", "run_instances_config")
+#
+# 	var count = 0
+# 	if instances_enabled:
+# 		count = run_config.size()
+# 	else:
+# 		count = 1
+#
+# 	_client_count_slider.value = count
+# 	_client_count_label.text = "%s" % count
 
 func _on_gs_start_pressed():
 	# Start client
@@ -172,5 +189,7 @@ func _on_gs_state_change(running: bool):
 	_gs_stop.visible = running
 	_gs_restart.visible = running
 
-func _on_customize_clients_pressed():
-	pass # Replace with function body.
+func _on_customize_instances_pressed():
+	if _instances_dialog != null:
+		# Reproduce native behavior: https://github.com/godotengine/godot/blob/48403b5358c11ffff702da82c48464db8c536ee3/editor/run_instances_dialog.cpp#L158
+		_instances_dialog.popup_centered(Vector2(1200, 600))
